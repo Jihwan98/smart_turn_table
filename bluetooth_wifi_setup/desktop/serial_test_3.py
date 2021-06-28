8739ff1e965249d21a27ff4a7413f372090dc2f3
Jihwan98 <76936390+Jihwan98@users.noreply.github.com>
2021-06-28 14:50:57 +0900

    Update serial_test_3.py

--- bluetooth_wifi_setup/desktop/serial_test_3.py
+++ bluetooth_wifi_setup/desktop/serial_test_3.py
@@ -1,14 +1,14 @@
 import serial
 import subprocess
 
-// wifi id 가져오기
+# wifi id 가져오기
 interface_output = subprocess.getstatusoutput("netsh wlan show interface")
 index1 = interface_output[1].index("SSID")
 ssid = interface_output[1][index1:].split(":")[1].split("\n")[0].strip()
 
 print(ssid)
 
-// wifi pw 가져오기
+# wifi pw 가져오기
 password_output = subprocess.getstatusoutput("netsh wlan show profiles {} key=clear".format(ssid))
 index2 = password_output[1].index("키 콘텐츠")
 password = password_output[1][index2:].split(":")[1].split("\n")[0].strip()
